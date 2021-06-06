ca1bc5bfa3678b551cac0d7e39166026592231c6
trongpv <trongpv@ghtk.vn>
2021-06-06 16:25:07 +0700

    add color log

--- build.pl
+++ build.pl
@@ -804,7 +804,7 @@ sub Clone($$)
 
    $repo_url_prefix =~ s,/*$,,;
 
-   print "Clone git : repo_name-> $repo_name, repo_branch_csv-> $repo_branch_csv, repo_tag_csv-> $repo_tag_csv, repo_remote-> $repo_remote, repo_url_prefix-> $repo_url_prefix";
+   print color('green') ."Clone git : repo_name-> $repo_name, repo_branch_csv-> $repo_branch_csv, repo_tag_csv-> $repo_tag_csv, repo_remote-> $repo_remote, repo_url_prefix-> $repo_url_prefix";
 
    my $repo_dir = "$CFG{BUILD_SOURCES_BASE_DIR}/$repo_name";
 
